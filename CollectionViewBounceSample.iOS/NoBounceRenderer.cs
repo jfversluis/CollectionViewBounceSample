10dce213f3cc97102da75a8cac030d5ed1b69952
Gerald Versluis <gerald@verslu.is>
2020-07-10 14:51:27 +0200

    Initial commit

---
+++ CollectionViewBounceSample.iOS/NoBounceRenderer.cs
@@ -0,0 +1,23 @@
+﻿using CollectionViewBounceSample.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CollectionView), typeof(NoBounceRenderer))]
+namespace CollectionViewBounceSample.iOS
+{
+    public class NoBounceRenderer : CollectionViewRenderer
+    {
+        public NoBounceRenderer()
+        {
+            
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<GroupableItemsView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+                Controller.CollectionView.Bounces = false;
+        }
+    }
+}
